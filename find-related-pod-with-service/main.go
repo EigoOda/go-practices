0060406c62b740b6ff31b1596053c08a892c06b7
dubs11kt <dubs11kt@gmail.com>
2022-09-25 22:48:09 +0900

    Add find-releated-pod-with-service lci

---
+++ find-related-pod-with-service/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+    "context"
+	"flag"
+	"fmt"
+    "os"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func main() {
+	kubeconfig := flag.String("kubeconfig",getEnv("KUBECONFIG", clientcmd.RecommendedHomeFile),"kubeconfig file")
+	flag.Parse()
+
+	// use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+    clientset, err := kubernetes.NewForConfig(config)
+    if err != nil {
+        panic(err.Error())
+    }
+
+    pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
+    if err != nil {
+        panic(err.Error())
+    }
+    fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+}
+
+func getEnv(key, fallback string) string {
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		return fallback
+	}
+	return value
+}
